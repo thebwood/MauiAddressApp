34e3e5a0019a6f1e69b21cdfa6461f1e81924858
Brian David Wood <brian_d_wood@yahoo.com>
2024-11-18 01:11:41 -0500

    Adding Address App .NET 8 version

---
+++ MauiAddressApp8/MauiAddressApp8.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAddressApp8.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
